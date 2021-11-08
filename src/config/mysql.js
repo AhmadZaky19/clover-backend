7138d22dda1b9a7143e0aa594ad22720eb603884
riyaraa <rinosatyaputra.id@gmail.com>
2021-11-08 16:40:45 +0700

    merge all branch

--- src/config/mysql.js
+++ src/config/mysql.js
@@ -1,17 +1,17 @@
 const mysql = require("mysql2");
 const connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "clover-hire",
+	host: "localhost",
+	user: "root",
+	password: "root",
+	database: "clover-hire",
 });
 
 connection.connect((err) => {
-  if (err) {
-    console.log("Failed to connect database...", err.message);
-  } else {
-    console.log("Successfully connect database...");
-  }
+	if (err) {
+		console.log("Failed to connect database...", err.message);
+	} else {
+		console.log("Successfully connect database...");
+	}
 });
 
 module.exports = connection;
