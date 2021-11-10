509acfef373e892718abec6a1f771c5ccd681b4d
riyaraa <rinosatyaputra.id@gmail.com>
2021-11-10 20:22:06 +0700

    Just a test commit to push new branch to heroku

--- src/config/redis.js
+++ src/config/redis.js
@@ -9,7 +9,11 @@ const redis = require("redis");
 // });
 
 // PRODUCTION
-const client = redis.createClient();
+const client = redis.createClient({
+	host: "redis-17644.c291.ap-southeast-2-1.ec2.cloud.redislabs.com",
+	password: "1JG90Us8B2fpZQAyKnczWp7Z9gLwv9Ox",
+	port: "17644",
+});
 
 client.on("connect", () => {
 	// eslint-disable-next-line no-console
