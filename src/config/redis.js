498856888ee00a23f8f3d91e23d26f3492f95747
riyaraa <rinosatyaputra.id@gmail.com>
2021-11-10 20:06:14 +0700

    Just a test commit to push new branch to heroku

--- src/config/redis.js
+++ src/config/redis.js
@@ -1,10 +1,17 @@
 const redis = require("redis");
 // require("dotenv").config();
 
+// const client = redis.createClient({
+// 	host: "127.0.0.1",
+// 	port: 6379,
+// 	password: "",
+// });
+
+// PRODUCTION
 const client = redis.createClient({
-	host: "127.0.0.1",
-	port: 6379,
-	password: "",
+	host: process.env.REDIS_HOSTNAME,
+	port: process.env.REDIS_PORT,
+	password: process.env.REDIS_PASSWORD,
 });
 
 client.on("connect", () => {
